8028c56109897095983a26d2da6042fccee488ed
chandan-banerjee <70322760+chandan-banerjee@users.noreply.github.com>
2020-09-01 20:32:00 +0530

    new proj

--- src/FirstComponent.js
+++ src/FirstComponent.js
@@ -8,7 +8,7 @@ constructor(props) {
 render() {
     const element = (<div>Text from Element created by Chandan</div>)
     return (<div className="comptext">
-    <h1 style="background-color:powderblue;">This is a heading from COMPONENT</h5>
+    <h1 style="background-color:powderblue">This is a heading from COMPONENT</h5>
     <h1>First Component</h1>
         {this.props.displaytext}
         {element}
